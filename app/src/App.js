5192a6ac4453eba305f4a0238509faa0e41a39a9
mixvert <bajkowski1e@gmail.com>
2023-12-30 10:16:48 +0100

    Added .env file and updated .gitignore

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import Appbar from "./Appbar/Appbar";
 import About from "./About/About";
+import Contact from "./Contact/Contact";
 
 export default function App () {
     return (
@@ -12,7 +13,7 @@ export default function App () {
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/work" element={<div>Work Page</div>}/>
                 <Route path="/about" element={<About/>}/>
-                <Route path="/contact" element={<div>Contact Page</div>}/>
+                <Route path="/contact" element={<Contact/>}/>
             </Routes>
         </Router>
     )
