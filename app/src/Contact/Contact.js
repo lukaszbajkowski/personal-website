8420e3a6d40f994922504a3580f9bb244fd5da07
mixvert <bajkowski1e@gmail.com>
2023-12-30 10:16:02 +0100

    Added .env file and updated .gitignore

---
+++ app/src/Contact/Contact.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Footer from "../LandingPage/Footer/Footer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import './Contact.scss'
+import {Grid, ThemeProvider} from "@mui/material";
+import theme from "../Style/Theme";
+import {Link} from "react-router-dom";
+import ArrowRightAltSharpIcon from "@mui/icons-material/ArrowRightAltSharp";
+import Form from "./Form";
+
+export default function Contact () {
+    return (
+        <ThemeProvider theme={theme}>
+            <Box className={`position-about`}>
+                <CssBaseline/>
+                <Toolbar className={`main-text-toolbar`}>
+                    <Typography
+                        variant={`h2`}
+                        component={`span`}
+                        className={`about-title`}
+                    >
+                        Talk to me.
+                    </Typography>
+                </Toolbar>
+                <Grid container
+                      spacing={0}
+                      className={`contact-grid`}
+                >
+                    <Grid item xs={12} md={6}>
+                        <Toolbar className={`contact-page-toolbar`}>
+                            <Typography
+                                variant={`h5`}
+                                component={`span`}
+                                className={`services-text`}
+                            >
+                                Want to say hello or have a question? Ping me.
+                            </Typography>
+                        </Toolbar>
+                        <Toolbar className={`contact-toolbar`}>
+                            <Link
+                                to={`mailto:contact.lukasz.bajkowski@gmail.com`}
+                                className={`mail-box`}
+                            >
+                                <Typography
+                                    className={`contact-link`}
+                                    component={`div`}
+                                >
+                            <span>
+                                Send me an email
+                            </span>
+                                    <ArrowRightAltSharpIcon className={`arrow-border`}/>
+                                </Typography>
+                            </Link>
+                        </Toolbar>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Form/>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Footer/>
+        </ThemeProvider>
+    );
+}
---
+++ app/src/Contact/Form.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {useForm, ValidationError} from '@formspree/react';
+import {Button} from "@mui/material";
+import ArrowRightAltSharpIcon from "@mui/icons-material/ArrowRightAltSharp";
+
+function ContactForm () {
+    const [state, handleSubmit] = useForm(process.env.REACT_APP_FORMSPREE_KEY);
+
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className={`form-width`}
+        >
+            <input
+                id="name"
+                type="text"
+                name="Full Name"
+                className={`email-input`}
+                placeholder="Full Name"
+                required
+            />
+            <ValidationError
+                prefix="Name"
+                field="name"
+                errors={state.errors}
+            />
+
+            <input
+                id="email"
+                type="email"
+                name="Email"
+                className={`email-input`}
+                placeholder="Email Address"
+                required
+            />
+            <ValidationError
+                prefix="Email"
+                field="email"
+                errors={state.errors}
+                className={`error-message`}
+            />
+
+            <textarea
+                id="message"
+                name="message"
+                rows={7}
+                placeholder="Message"
+                className={`email-area`}
+                required
+            />
+            <ValidationError
+                prefix="Message"
+                field="message"
+                errors={state.errors}
+            />
+
+            <Button
+                type="submit"
+                disabled={state.submitting}
+                className={`btn-send-form`}
+            >
+                <span>
+                    Send message
+                </span>
+                <ArrowRightAltSharpIcon className={`arrow-border`}/>
+            </Button>
+        </form>
+    );
+}
+
+function Form () {
+    return (
+        <ContactForm/>
+    );
+}
+
+export default Form;
