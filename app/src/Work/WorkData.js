e61562a3de1355bf92815a29e9dd6fdb41135cd4
mixvert <bajkowski1e@gmail.com>
2024-01-06 01:58:52 +0100

    Creation of a background for the title section of the single work page and renaming of the photos

--- app/src/Work/WorkData.js
+++ app/src/Work/WorkData.js
@@ -1,8 +1,8 @@
 import React from "react";
-import img1 from '../Img/Zrzut ekranu 2023-01-20 o 15.56.31.png';
-import img2 from '../Img/Zrzut ekranu 2023-01-20 o 23.31.47.png';
-import img3 from '../Img/Zrzut ekranu 2023-01-20 o 15.56.38.png';
-import img4 from '../Img/Zrzut ekranu 2023-01-21 o 11.20.00.png';
+import img1 from '../Img/Zrzut_ekranu_2023-01-20_o_15.56.31.png';
+import img2 from '../Img/Zrzut_ekranu_2023-01-20_o_23.31.47.png';
+import img3 from '../Img/Zrzut_ekranu_2023-01-20_o_15.56.38.png';
+import img4 from '../Img/Zrzut_ekranu_2023-01-21_o_11.20.00.png';
 
 
 export const workData = [
--- app/src/WorkDetails/WorkDetails.js
+++ app/src/WorkDetails/WorkDetails.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
+import './WorkDetails.scss'
+import {workData} from "../Work/WorkData";
 
 const WorkDetails = () => {
     const {id} = useParams();
+    const selectedWork = workData[id - 1];
 
-    // Pobierz dane dla konkretnego projektu z workData
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0) 30%, ${selectedWork.color})`,
+    };
 
     return (
-        <div>
-            <h2>Details for Work {id}</h2>
-            {/* Wyświetl szczegóły projektu */}
-        </div>
+
+        <section
+            className="background-section"
+            style={{backgroundImage: `url(${selectedWork.imgSrc})`,}}
+            data-radium="true"
+        >
+            <div
+                data-radium="true"
+                className={`background-style`}
+                style={backgroundStyle}
+            ></div>
+            <div className="container-background container" data-radium="true">
+                <div className="background-text" data-radium="true">
+                    <div className="text-position" data-radium="true">
+                        <h1 className="text-size" data-radium="true">
+                            {selectedWork.title}
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
