e531211c96d47aba448379f852bd1be10ca7c6bd
mixvert <bajkowski1e@gmail.com>
2024-01-12 23:57:18 +0100

    Completion of data on author's works

--- app/src/Work/WorkData.js
+++ app/src/Work/WorkData.js
@@ -89,7 +89,7 @@ export const workData = [
         },
         about_img: about_img1,
         about_img_alt: "Pearson Course shown on multiple devices",
-        related_posts:[1,2,3],
+        related_posts: [1, 2, 3],
     },
     {
         imgSrc: img2,
@@ -97,29 +97,21 @@ export const workData = [
         subtitle: 'Pearson',
         content: "Creation of a new way in educating a workforce remotely for Pearson Education.",
         color: '#00A9C6FF',
-    },
-    {
-        imgSrc: img3,
-        title: 'People Search App',
-        subtitle: 'AstraZeneca',
-        content: "An iOS app for searching colleagues within AstraZeneca.",
-        color: '#C90077FF',
+        project_logo: logo_img1,
         intro: 'Pearson wanted a new way to educate a workforce and increase their reach across a larger audience.',
         intro_desc: {
             paragraph1: "Current safety in the workplace courses consist of 800+ powerpoint slides shoved into two full working days. Employees are often forced into the course and made to leave work for these two days.",
             paragraph2: "Our aim was to bring some enjoyment back into learning and make the course more insightful for both employers and employees – whilst still being IOSH accredited.",
-        }
-    },
-    {
-        imgSrc: img4,
-        title: 'Metric Typeface',
-        subtitle: 'Side Project',
-        content: "A geometric display typeface built for unique brands.",
-        color: '#00BC6CFF',
-        intro: 'Pearson wanted a new way to educate a workforce and increase their reach across a larger audience.',
-        intro_desc: {
-            paragraph1: "Current safety in the workplace courses consist of 800+ powerpoint slides shoved into two full working days. Employees are often forced into the course and made to leave work for these two days.",
-            paragraph2: "Our aim was to bring some enjoyment back into learning and make the course more insightful for both employers and employees – whilst still being IOSH accredited.",
+        },
+        involvement: 'My involvement consisted of being the sole designer, overseeing the art direction and design. This included the creation of wireframes, product maps, prototypes including leading the frontend development for production – whilst dealing with the client day-to-day using an agile process.',
+        involvement_elements: {
+            element1: "Workflows + product mapping",
+            element2: "Wireframing",
+            element3: "Art + film direction",
+            element4: "Lead design",
+            element5: "Illustration",
+            element6: "Lead frontend development",
+            element7: "Lead frontend development",
         },
         process: {
             element1: {
@@ -158,5 +150,176 @@ export const workData = [
                 }
             }
         },
+        process_title: "Brief: Transformation from an 800+ powerpoint to an energetic and educational product.",
+        process_desc: {
+            element1: "The initial brief was to create an engaging educational course that didn’t shackle employees into a room for two days. The key features for the new course were; to reduce text based content, make the course accessible from any device and to make it interactive and enjoyable.",
+            element2: "By making the course responsive and work on every device, employees could spread the course out over a month – digesting the content at their own pace.",
+        },
+        process_img: process_img1,
+        process_img_alt: "Pearson Education Course shown on multiple devices",
+        about_background_video: vid1,
+        about_title: "Through gamification users were challenged to want to learn.",
+        about_desc: {
+            element1: "Through gamification quick quiz called Fasttrack was created which concludes each module. Fasttrack helped break up heavy amounts of content with fun and varied games, but also ensured students were learning the content.",
+            element2: "The course had to meet standards set by IOSH (safety regulation board) for accreditation. Fasttrack was made up of multiple games, ranging from drag and drop into the missing fields, fruit ninja style slashing of incorrect answers or stack the answers in the correct order.",
+            element3: "Each game had their own brand, including targeted voiceovers encouraging the students to fully engage with the course.",
+        },
+        about_img: about_img1,
+        about_img_alt: "Pearson Course shown on multiple devices",
+        related_posts: [1, 2, 3],
+    },
+    {
+        imgSrc: img3,
+        title: 'People Search App',
+        subtitle: 'AstraZeneca',
+        content: "An iOS app for searching colleagues within AstraZeneca.",
+        color: '#C90077FF',
+        project_logo: logo_img1,
+        intro: 'Pearson wanted a new way to educate a workforce and increase their reach across a larger audience.',
+        intro_desc: {
+            paragraph1: "Current safety in the workplace courses consist of 800+ powerpoint slides shoved into two full working days. Employees are often forced into the course and made to leave work for these two days.",
+            paragraph2: "Our aim was to bring some enjoyment back into learning and make the course more insightful for both employers and employees – whilst still being IOSH accredited.",
+        },
+        involvement: 'My involvement consisted of being the sole designer, overseeing the art direction and design. This included the creation of wireframes, product maps, prototypes including leading the frontend development for production – whilst dealing with the client day-to-day using an agile process.',
+        involvement_elements: {
+            element1: "Workflows + product mapping",
+            element2: "Wireframing",
+            element3: "Art + film direction",
+            element4: "Lead design",
+            element5: "Illustration",
+            element6: "Lead frontend development",
+            element7: "Lead frontend development",
+        },
+        process: {
+            element1: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element2: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element3: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element4: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element5: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            }
+        },
+        process_title: "Brief: Transformation from an 800+ powerpoint to an energetic and educational product.",
+        process_desc: {
+            element1: "The initial brief was to create an engaging educational course that didn’t shackle employees into a room for two days. The key features for the new course were; to reduce text based content, make the course accessible from any device and to make it interactive and enjoyable.",
+            element2: "By making the course responsive and work on every device, employees could spread the course out over a month – digesting the content at their own pace.",
+        },
+        process_img: process_img1,
+        process_img_alt: "Pearson Education Course shown on multiple devices",
+        about_background_video: vid1,
+        about_title: "Through gamification users were challenged to want to learn.",
+        about_desc: {
+            element1: "Through gamification quick quiz called Fasttrack was created which concludes each module. Fasttrack helped break up heavy amounts of content with fun and varied games, but also ensured students were learning the content.",
+            element2: "The course had to meet standards set by IOSH (safety regulation board) for accreditation. Fasttrack was made up of multiple games, ranging from drag and drop into the missing fields, fruit ninja style slashing of incorrect answers or stack the answers in the correct order.",
+            element3: "Each game had their own brand, including targeted voiceovers encouraging the students to fully engage with the course.",
+        },
+        about_img: about_img1,
+        about_img_alt: "Pearson Course shown on multiple devices",
+        related_posts: [1, 2, 3],
+    },
+    {
+        imgSrc: img4,
+        title: 'Metric Typeface',
+        subtitle: 'Side Project',
+        content: "A geometric display typeface built for unique brands.",
+        color: '#00BC6CFF',
+        project_logo: logo_img1,
+        intro: 'Pearson wanted a new way to educate a workforce and increase their reach across a larger audience.',
+        intro_desc: {
+            paragraph1: "Current safety in the workplace courses consist of 800+ powerpoint slides shoved into two full working days. Employees are often forced into the course and made to leave work for these two days.",
+            paragraph2: "Our aim was to bring some enjoyment back into learning and make the course more insightful for both employers and employees – whilst still being IOSH accredited.",
+        },
+        involvement: 'My involvement consisted of being the sole designer, overseeing the art direction and design. This included the creation of wireframes, product maps, prototypes including leading the frontend development for production – whilst dealing with the client day-to-day using an agile process.',
+        involvement_elements: {
+            element1: "Workflows + product mapping",
+            element2: "Wireframing",
+            element3: "Art + film direction",
+            element4: "Lead design",
+            element5: "Illustration",
+            element6: "Lead frontend development",
+            element7: "Lead frontend development",
+        },
+        process: {
+            element1: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element2: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element3: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element4: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            },
+            element5: {
+                title: "Discover",
+                elements: {
+                    element1: "Client workshops",
+                    element2: "Current market alternatives",
+                }
+            }
+        },
+        process_title: "Brief: Transformation from an 800+ powerpoint to an energetic and educational product.",
+        process_desc: {
+            element1: "The initial brief was to create an engaging educational course that didn’t shackle employees into a room for two days. The key features for the new course were; to reduce text based content, make the course accessible from any device and to make it interactive and enjoyable.",
+            element2: "By making the course responsive and work on every device, employees could spread the course out over a month – digesting the content at their own pace.",
+        },
+        process_img: process_img1,
+        process_img_alt: "Pearson Education Course shown on multiple devices",
+        about_background_video: vid1,
+        about_title: "Through gamification users were challenged to want to learn.",
+        about_desc: {
+            element1: "Through gamification quick quiz called Fasttrack was created which concludes each module. Fasttrack helped break up heavy amounts of content with fun and varied games, but also ensured students were learning the content.",
+            element2: "The course had to meet standards set by IOSH (safety regulation board) for accreditation. Fasttrack was made up of multiple games, ranging from drag and drop into the missing fields, fruit ninja style slashing of incorrect answers or stack the answers in the correct order.",
+            element3: "Each game had their own brand, including targeted voiceovers encouraging the students to fully engage with the course.",
+        },
+        about_img: about_img1,
+        about_img_alt: "Pearson Course shown on multiple devices",
+        related_posts: [1, 2, 3],
     },
 ];
