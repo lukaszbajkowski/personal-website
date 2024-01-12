b965158c0aa1e639716e44fabb1f7a36af0614c5
mixvert <bajkowski1e@gmail.com>
2024-01-13 00:05:05 +0100

    Optimizing elements of the Work page

--- app/src/Work/WorkGrid.js
+++ app/src/Work/WorkGrid.js
@@ -1,38 +1,49 @@
 import React from 'react';
 import {Box, Grid} from '@mui/material';
 import './Work.scss';
-import {workData} from "./WorkData";
-import {Link} from "react-router-dom";
+import {workData} from './WorkData';
+import {Link} from 'react-router-dom';
 
-const ImageComponent = ({src, id, alt, title, subtitle}) => (
-    <Grid item xs={12} md={4} className={`work-item`}>
-        <Link
-            to={`/work/${id}`}
-        >
+const ImageComponent = React.memo(({id, src, alt, title, subtitle}) => (
+    <Grid
+        item
+        xs={12}
+        md={4}
+        className={`work-item`}
+    >
+        <Link to={`/work/${id}`}>
             <Box className={`work-box`}>
                 <div className="square-container">
-                    <img src={src} alt={alt} className={`work-img`}/>
-                    <h2 data-radium="true" className={`title-work`}>
+                    <img
+                        src={src}
+                        alt={alt}
+                        className={`work-img`}
+                    />
+                    <h2 className={`title-work`}>
                         {title}
                     </h2>
-                    <span data-radium="true" className={`subtitle-work`}>
+                    <span className={`subtitle-work`}>
                         {subtitle}
                     </span>
                 </div>
             </Box>
         </Link>
     </Grid>
-);
+));
 
 const WorkGrid = () => {
     return (
-        <Grid container spacing={2} className={`work-grid`}>
-            {workData.map((data, index) => (
+        <Grid
+            container
+            spacing={2}
+            className={`work-grid`}
+        >
+            {workData.map((data) => (
                 <ImageComponent
-                    key={index}
-                    id={index + 1}
+                    key={data.id}
+                    id={data.id}
                     src={data.imgSrc}
-                    alt={`Work ${index + 1}`}
+                    alt={`Work ${data.id}`}
                     title={data.title}
                     subtitle={data.subtitle}
                 />
